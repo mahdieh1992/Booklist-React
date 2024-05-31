6728b30a8fe925a8e02adc3733a6c063120e3c46
mahdieh1992 <mohamadimahdieh70@gmail.com>
2024-05-31 12:37:20 +0330

    project book list

--- src/Components/BookForm.js
+++ src/Components/BookForm.js
@@ -6,7 +6,56 @@ export default class BookForm extends React.Component {
     constructor(props) {
         super()
 
+        this.state = {
+            title: '',
+            author: '',
+            year: '',
+            addBook: false,
+            booksList: []
+        }
 
+        this.titleHandle = this.titleHandle.bind(this)
+        this.authorHandle = this.authorHandle.bind(this)
+        this.yearHandle = this.yearHandle.bind(this)
+        this.addBookHandle = this.addBookHandle.bind(this)
+    }
+
+    titleHandle(event) {
+        this.setState({
+            title: event.target.value
+        })
+    }
+
+    authorHandle(event) {
+        this.setState(
+            {
+                author: event.target.value
+            }
+        )
+    }
+
+    yearHandle(event) {
+        this.setState({
+            year: event.target.value
+        })
+
+    }
+
+    addBookHandle(event) {
+        event.preventDefault()
+        this.setState({
+            addBook: true
+        })
+        if (this.state.title.length !== 0 && this.state.author.length !== 0 && this.state.year.length !== 0) {
+            let title = this.state.title
+            let author = this.state.author
+            let year = this.state.year
+            let currentList=[{id:this.state.booksList.length+1, title: title, author: author, year: year}]
+            this.setState((prevState)=>{
+                return {booksList:[...prevState.booksList,...currentList]}
+            })
+
+        }
     }
 
     render() {
@@ -14,17 +63,42 @@ export default class BookForm extends React.Component {
             <div className="Book">
                 <form className="BookForm">
                     <label className='BookForm__lbl'>Tile</label>
-                    <input type="text" className='BookForm__in' ></input>
+                    <input type="text" className='BookForm__in' value={this.state.title} onChange={(event) => { this.titleHandle(event) }}></input>
+                    {this.state.addBook && this.state.title.length === 0 && (<span className="spanValid">Please Enter Title</span>)}
 
                     <label className='BookForm__lbl'>Author</label>
-                    <input type="text" className='BookForm__in'></input>
+                    <input type="text" className='BookForm__in' value={this.state.author} onChange={(event) => { this.authorHandle(event) }}></input>
+                    {this.state.addBook && this.state.author.length === 0 && (
+                        <span className="spanValid">Please Enter Author</span>
+                    )}
 
                     <label className='BookForm__lbl'>Year</label>
-                    <input type="text" className='BookForm__in' ></input>
-
-                    <button className='BookForm__Add'>Add Book</button>
-
+                    <input type="text" className='BookForm__in' value={this.state.year} onChange={(event) => { this.yearHandle(event) }}></input>
+                    {this.state.addBook && this.state.year.length === 0 && (
+                        <span className="spanValid">Please Enter Year</span>
+                    )}
+                    <button className='BookForm__Add' value={this.state.addBook} onClick={(event) => this.addBookHandle(event)}>Add Book</button>
+                    {console.log(this.state.booksList)}
                 </form>
+                <div className="BookAdded">
+                    <table className="BookAdded__Tbl">
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Year</th>
+                        </tr>
+                       
+                            {this.state.booksList.map((book)=>(
+                                    <tr>
+                                        <td>{book.title}</td>
+                                        <td>{book.author}</td>
+                                        <td>{book.year}</td>
+                                    </tr>
+                            ))}
+                        
+                    
+                    </table>
+                </div>
             </div>
         )
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -14,25 +14,6 @@ export default class Main extends React.Component {
         return (
             <div className="main">
                 <BookForm />
-                <div className="BookAdded">
-                    <table className="BookAdded__Tbl">
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Year</th>
-                        </tr>
-                        <tr>
-                            <td>Peter</td>
-                            <td>Griffin</td>
-                            <td>$100</td>
-                        </tr>
-                        <tr>
-                            <td>Lois</td>
-                            <td>Griffin</td>
-                            <td>$150</td>
-                        </tr>
-                    </table>
-                    </div>
             </div>
 
         )
